799d444dd8d1b28158a7740207208e8e1eab16d9
shoptacle <accounts@shoptacle.com>
2023-03-08 07:54:37 +0100

    image fix

--- pages/embedded/index.js
+++ pages/embedded/index.js
@@ -54,7 +54,7 @@ export default function Index() {
               },
             ]}
           >
-            <Image
+            <img
               alt=""
               width="100%"
               height="100%"
